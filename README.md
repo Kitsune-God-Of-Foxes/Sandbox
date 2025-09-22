fc827b0ee693723dee63106e8ffcc80a5e5f4147
Kitsune-God-Of-Foxes <Sebastian.p.payne@Icloud.com>
2025-09-22 16:08:10 +0000

    If else block motherf***ers

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# AP CSP Sandbox
-Use this sandbox for short coding exercises that you don't need to submit.
+"""
+AP computer sciences sandbox
+store stuff here
+"""
+
+def main() -> None:
+    total_students: int = int(input())
+    group size: int = int(input())
+
+
+
+if__name__ == "__main__":
+ main()
--- sandbox.py
+++ sandbox.py
@@ -4,7 +4,16 @@ This is a repo you can use to store small warm-up problems and try out code.
 """
 
 def main() -> None:
-  pass
+  total_students: int = int(input())
+  group_size: int = int(input())
+  num_groups: int = int(total_students // group_size)
+  leftovers: int = int(total_students % group_size)
+  
+  print(f"{num_groups} of {group_size} were formed.)")
+  if leftovers > 1 or leftovers < 1:
+    print(f"{leftovers} people were not in a group")
+  else:
+    print(f"{leftovers} person was not in a group")
 
 if __name__ == "__main__":
   main()
