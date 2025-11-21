8a4c58764d3e775150ef051c3ae1fe5fab5c75a1
Kitsune-God-Of-Foxes <Sebastian.p.payne@Icloud.com>
2025-11-21 17:08:28 +0000

    HELLO WORLD!

--- sandbox.py
+++ sandbox.py
@@ -4,16 +4,13 @@ This is a repo you can use to store small warm-up problems and try out code.
 """
 
 def main() -> None:
-  total_students: int = int(input())
-  group_size: int = int(input())
-  num_groups: int = int(total_students // group_size)
-  leftovers: int = int(total_students % group_size)
+  import sys
+  import math
+  import string
   
-  print(f"{num_groups} of {group_size} were formed.)")
-  if leftovers > 1 or leftovers < 1:
-    print(f"{leftovers} people were not in a group")
-  else:
-    print(f"{leftovers} person was not in a group")
+  cases = int(sys.stdin.readline().rstrip())
+  for caseNum in range(cases):
+    print(sys.stdin.readline().rstrip())
 
 if __name__ == "__main__":
   main()
