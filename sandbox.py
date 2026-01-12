75225e2f389e6e5b9122486cac15c7601421d179
Kitsune-God-Of-Foxes <Sebastian.p.payne@Icloud.com>
2026-01-12 17:20:03 +0000

    Color wheel

--- sandbox.py
+++ sandbox.py
@@ -2,20 +2,18 @@
 AP Computer Science Principles Sandbox
 This is a repo you can use to store small warm-up problems and try out code.
 """
+import sys
+import math
+import string
 
 def main() -> None:
-  N = int(input())
-  compOG = input()
-  number, S, W, P, C = compOG.split()
-  Value = S + W + P + C
-  for i in range: N
-  altcomp = input()
-  altnumber, altS, altW, altP, altC, = altcomp.split()
-  Altvalue = altS + altW + altP + altC
-  if Value + 1.2 < Altvalue:
-    Value = Altvalue
-    number = altnumber
-  print(number, Value)
+  case_count: int = int(sys.stdin.readline().rstrip())  
+  for _ in range(case_count):
+    color: str = sys.stdin.readline().rstrip()
+
+
+
+  
 
 if __name__ == "__main__":
   main()
