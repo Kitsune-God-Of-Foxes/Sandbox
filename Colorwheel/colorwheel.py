56f5a1b5a730dcd41d4e761d2349a462cbd5fe29
Kitsune-God-Of-Foxes <Sebastian.p.payne@Icloud.com>
2026-01-23 16:58:39 +0000

    finished colorwheel

--- Colorwheel/colorwheel.py
+++ Colorwheel/colorwheel.py
@@ -6,14 +6,17 @@ def main() -> None:
   case_count: int = int(sys.stdin.readline().rstrip())  
   for _ in range(case_count):
     color: str = sys.stdin.readline().rstrip()
-  if color == "red" or color == "blue" or color =="yellow":
-    print(F"No colors need to be mixed to make {color}.")
-  elif color == "orange" or color == "red-orange" or color == "yellow-orange":
-    print(F"In order to make {color}, red and yellow must be mixed.")
-  elif color == "green" or color == "blue-green" or color == "yellow-green":
-    print(F"In order to make {color} blue and yellow must be mixed.")
-  else:
-    print(F" In order to make {color} blue and red must be mixed.")
+    if color == "red" or color == "blue" or color =="yellow":
+      print(F"No colors need to be mixed to make {color}.")
+    elif color == "orange" or color == "red-orange" or color == "yellow-orange":
+      print(F"In order to make {color}, red and yellow must be mixed.")
+    elif color == "green" or color == "blue-green" or color == "yellow-green":
+      print(F"In order to make {color} blue and yellow must be mixed.")
+    else:
+      print(F" In order to make {color} blue and red must be mixed.")
+
+if __name__ == "__main__":
+  main()
 
 
 
