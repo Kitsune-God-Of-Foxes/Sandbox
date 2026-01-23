7b40451289856fbb7db97c4f936cee09c4f82c56
Kitsune-God-Of-Foxes <Sebastian.p.payne@Icloud.com>
2026-01-23 17:22:08 +0000

    finished swap

--- SwapC/SWAPC.py
+++ SwapC/SWAPC.py
@@ -16,6 +16,7 @@ def main() -> None:
     OrigP = int(OrigP)
     OrigC = int(OrigC)
     CurrentSwapC = OrigS + OrigW + OrigP + OrigC
+    OriginalSwapC = CurrentSwapC
     CurrentSwapC = int(CurrentSwapC)
     for _ in range(Number_of_cases):
         New_component = str(sys.stdin.readline().rstrip())
@@ -25,7 +26,17 @@ def main() -> None:
         NewP = int(NewP)
         NewC = int(NewC)
         NewSwapC = NewS + NewW + NewP + NewC
-        if NewSwapC > CurrentSwapC:
+        CurrentDecimal = CurrentSwapC % 1
+        NewDecimal = NewSwapC % 1
+        if NewDecimal >= 0.5:
+            NewSwapC = math.ceil(NewSwapC)
+        else:
+            NewSwapC = math.floor(NewSwapC)
+        if CurrentDecimal >= 0.5:
+            CurrentSwapC = math.ceil(CurrentSwapC)
+        else:
+            NewSwapC = math.floor(NewSwapC)
+        if NewSwapC < OriginalSwapC * 0.8 and NewSwapC < CurrentSwapC:
             CurrentSwapC = NewSwapC
             Current_number = NewNumber
     print(Current_number, CurrentSwapC)
